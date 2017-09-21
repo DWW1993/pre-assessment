9fdf0f69c8cddd8d24231c499fff785e43cdcdaa
DWW1993 <daniel.w.walker1993@gmail.com>
2017-09-21 16:50:04 -0500

    final

---
+++ java.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    $('.FEATURES').on('click', function(){
+        alert('This does not work');
+    });
+
+    $('.ABOUT').on('click', function(){
+        alert('This does not work either');
+    });
+
+    $('.STORE').on('click', function(){
+        alert('These buttons do not work my dude');
+    });
+
+    $('.BLOG').on('click', function(){
+        alert('Stop clicking these buttons, for they do not work');
+    });
+
+    $('.LEARN').on('click', function(){
+        alert('There is no more to learn');
+    });
+
+    $('.MORE').on('click', function(){
+        alert('Nope');
+    });
+})
